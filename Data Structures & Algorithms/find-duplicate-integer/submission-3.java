a1ef3d7fae11275759293172dcec3c5345a7a335
Keegan Gerrard <keegan1168@gmail.com>
2026-04-04 15:20:02 +0800

    Add: find-duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> hash = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(hash.contains(nums[i]))return nums[i];
+            hash.add(nums[i]);
+        }
+        return 0;
+    }
+}
